2d2b640b1022bb55b064bd3648cfe7b9ed2e8c70
Andrey Shamurzin <andrey.shamurzin@solidlab.ru>
2025-11-09 22:22:51 +0300

    Add API with postgres db

---
+++ web/views.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from psql import requests_count, add_ip_request
+
+
+app = Flask(__name__)
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    add_ip_request(request.remote_addr)
+    return "pong\n"
+
+@app.route("/visits", methods=["GET"])
+def visits():
+    return f"{requests_count()}"
