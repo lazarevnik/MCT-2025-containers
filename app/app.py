6545e332f61e756c877b7fc38da3f537d63401e2
bikmish <bikmish@ispras.ru>
2025-11-11 17:21:07 +0300

    Removing redundant db check in app (using only sql init script + docker compose health check)

--- app/app.py
+++ app/app.py
@@ -10,26 +10,6 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
-def check_db_connection(max_retries=15, delay=3):
-    for i in range(max_retries):
-        try:
-            conn = psycopg2.connect(
-                host=os.getenv('DB_HOST', 'db'),
-                database=os.getenv('DB_NAME', 'postgres'),
-                user=os.getenv('DB_USER', 'postgres'),
-                password=os.getenv('DB_PASSWORD', 'password'),
-                connect_timeout=3
-            )
-            conn.close()
-            logger.info("Connected to DB!")
-            return True
-        except psycopg2.OperationalError as e:
-            logger.warning(f"Failed to connect to DB :( Attempt {i + 1}/{max_retries}): {e}")
-            if i < max_retries - 1:
-                time.sleep(delay)
-    return False
-
-
 def get_db_connection():
     conn = psycopg2.connect(
         host=os.getenv('DB_HOST', 'db'),
@@ -64,9 +44,5 @@ def visits():
 
 
 if __name__ == '__main__':
-    if not check_db_connection():
-        logger.error("Could not connect to DB")
-        exit(1)
-
     logger.info("Starting flask app")
     app.run(host='0.0.0.0', port=5000)
