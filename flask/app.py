dddcc0097dccfec6d2fde79f34f39002076646ac
YegorCrazy <egor2002288@gmail.com>
2025-11-12 01:50:34 +0800

    task first attempt

---
+++ flask/app.py
@@ -0,0 +1,58 @@
+from os import environ
+from flask import Flask, request
+import psycopg2
+
+app = Flask(__name__)
+
+password = "mysecretpassword"
+
+@app.route('/ping')
+def ping():
+    db_host = environ.get('DB_HOST')
+    conn = psycopg2.connect(database="ping_db",
+                            user="postgres",
+                            password=password,
+                            host=db_host, port="5432")
+    cur = conn.cursor()
+
+    client_ip = request.remote_addr
+
+    cur.execute(
+        f'''INSERT INTO ips (client_ip, call_count) \
+        VALUES ('{client_ip}', 1) \
+        ON CONFLICT (client_ip) \
+        DO UPDATE SET call_count = ips.call_count + 1;''')
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return 'pong'
+
+@app.route('/visits')
+def visits():
+    db_host = environ.get('DB_HOST')
+    conn = psycopg2.connect(database="ping_db",
+                            user="postgres",
+                            password=password,
+                            host=db_host, port="5432")
+    cur = conn.cursor()
+
+    client_ip = request.remote_addr
+
+    cur.execute(
+        f'''SELECT COALESCE( \
+            (SELECT call_count FROM ips WHERE client_ip = '{client_ip}'), \
+            0 \
+        ) AS call_count;''')
+    
+    count = cur.fetchone()[0]
+
+    cur.close()
+    conn.close()
+
+    return str(count)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
