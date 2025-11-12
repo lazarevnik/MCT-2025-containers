97fe4fff064fa96a5541eec65b96346fc053eb8c
Krakenrek <43118568+Krakenrek@users.noreply.github.com>
2025-11-12 15:37:38 +0300

    I believe in myself

--- tests.py
+++ tests.py
@@ -73,4 +73,4 @@ def test_count_visits_without_database():
     app.state.database = ""
     from server import count_visits
     result = count_visits()
-    assert result is None
+    assert result.text == "0"
