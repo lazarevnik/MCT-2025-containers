c37e51036da377ce542c2e0405a098af5a873d81
greenbunny108 <mpopov@ispras.ru>
2025-11-14 16:10:40 +0300

    docker init db fixes

--- init-db/init-db.py
+++ init-db/init-db.py
@@ -6,16 +6,22 @@ DB_NAME = os.getenv("DB_NAME", "app")
 DB_USER = os.getenv("DB_USER", "user")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 
-conn = psycopg2.connect(
-    host=DB_HOST,
-    dbname=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD,
-)
-cur = conn.cursor()
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS visits (id SERIAL PRIMARY KEY, ip VARCHAR(64) NOT NULL)"
-)
-conn.commit()
-cur.close()
-conn.close()
+
+def main():
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS visits (id SERIAL PRIMARY KEY, ip VARCHAR(64) NOT NULL)"
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
