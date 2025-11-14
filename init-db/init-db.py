e321972fa085741bd23448fdbf0d9d05cf9ebab2
greenbunny108 <mpopov@ispras.ru>
2025-11-14 16:36:52 +0300

    add retry for db-init

--- init-db/init-db.py
+++ init-db/init-db.py
@@ -1,4 +1,5 @@
 import os
+import time
 import psycopg2
 
 DB_HOST = os.getenv("DB_HOST", "db")
@@ -8,12 +9,21 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 
 
 def main():
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-    )
+    conn = None
+    for _ in range(30):
+        try:
+            conn = psycopg2.connect(
+                host=DB_HOST,
+                dbname=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD,
+            )
+            break
+        except psycopg2.OperationalError:
+            time.sleep(1)
+    if conn is None:
+        raise SystemExit(1)
+
     cur = conn.cursor()
     cur.execute(
         "CREATE TABLE IF NOT EXISTS visits (id SERIAL PRIMARY KEY, ip VARCHAR(64) NOT NULL)"
